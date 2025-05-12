73661009ee5f7547bca8f1c48fb80a2b40fd20e6
Adrian <adriandavidcc@gmail.com>
2025-05-11 21:08:44 -0500

    Se añadió el get para categorias, y una busqueda por id

--- src/routes/categoria.js
+++ src/routes/categoria.js
@@ -10,5 +10,23 @@ router.post("/categoria", (req, res) => {
         .then((data) => res.json(data))
         .catch((error) => res.status(500).json({ message: error.message }));
 });
+router.get("/categoria", (req, res) => {
+    categoriaSchema.find()
+        .then((data) => res.json(data))
+        .catch((error) => res.status(500).json({ message: error.message }));
+});
 
+
+router.get("/categoria/:id", (req, res) => {
+    const { id } = req.params;
+    categoriaSchema
+        .findById(id)
+        .then((data) => {
+            if (!data) {
+                return res.status(404).json({ message: "Categoria no encontrada" });
+            }
+            res.json(data);
+        })
+        .catch((error) => res.status(500).json({ message: error.message }));
+});
 module.exports = router;
