faec7be85f5bf97ecb1c8cb64da70cb8dec21edf
Adrian <adriandavidcc@gmail.com>
2025-05-11 20:47:13 -0500

    Se define la estructura basica del proyecto, se añaden los archivos para el modulo "categorias"

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const parser = require("body-parser");
+const express = require('express');
+const app = express();
+const port = 3000;
+const mongoose = require("mongoose");
+require('dotenv').config();
+
+app.use(parser.urlencoded({ extended: false })); 
+app.use(parser.json()); 
+app.use(express.json());
+mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() => console.log("Conexión exitosa"))
+    .catch((error) => console.log(error));
+
+// Conexión al puerto
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
