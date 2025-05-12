fda92346c71eba534a1c8d737bde099f57460a90
Luna Bustos <lunabustos911@gmail.com>
2025-05-11 19:18:05 -0500

    agregamos el campo revision que almacena un id de una noticia en otro esquema

--- src/models/noticia.js
+++ src/models/noticia.js
@@ -19,4 +19,17 @@ const noticiaSchema = mongoose.Schema({
     Contenido: {
         type: String,
         required: true,
+    },
+    revision: { 
+        type: mongoose.Schema.Types.ObjectId, //aqui almacenas un ID de una noticia en otro esquema
+        ref: "revision", // este es el esquema que se llama (ese ref es que referencia a otro esquema)
+        required: true, // esto significa que si o si se tiene que tener una revision
+    }, 
+    revisó: {
+        type: String,
+        required: true,
+    },
+    imagen: {
+        type: String,
+        required: true,
     }});
