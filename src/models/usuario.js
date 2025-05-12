23841b73de2a9191518873256ed522952a6431b7
Santiago Prieto <santyprieto1080@gmail.com>
2025-05-11 21:55:20 -0500

    Revisando el esquema de usuario: Campos requeridos y validaciones

---
+++ src/models/usuario.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true 
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: /.+\@.+\..+/
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    enum: ['usuario', 'autor', 'editor', 'revisor'],
+    default: 'usuario'
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model('User', userSchema);
