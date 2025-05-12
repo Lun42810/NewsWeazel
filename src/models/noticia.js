3bdf414f1aec0ac4a0fdf6edd52d7bbac4dbf353
Adrian <adriandavidcc@gmail.com>
2025-05-11 21:16:39 -0500

    Se añade un intento de la conexión para asociar una categoria de este modelo con una noticia en el otro modelo

---
+++ src/models/noticia.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose"); // importando el componente mogoose
+const noticiaSchema = mongoose.Schema({
+    categoria: {
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'categoria',
+        required: true, 
+    },
+});
+module.exports = mongoose.model("Noticia", noticiaSchema);
