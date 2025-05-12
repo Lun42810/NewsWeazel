0921cde0f0ee4422b4b5c39d4420953dee9b68d7
Luna Bustos <lunabustos911@gmail.com>
2025-05-11 19:55:06 -0500

    se agrego el metodo de actualizar noticia segun los parametros que en el modelo se establecieron

--- src/routes/noticia.js
+++ src/routes/noticia.js
@@ -45,3 +45,14 @@ router.get('/categoria/:categoriaId', async (req, res) => {
         res.status(500).json({ error: 'Error al obtener noticias por categoría' }); 
     }
 });
+// Actualizar una noticia por ID
+router.put("/noticia/:id", (req, res) => {
+    const { id } = req.params;
+    const { titulo, autor, fechapublicación, encabezado, Contenido, revisada, revisó, imagen, categoria } = req.body;
+    Noticia.updateOne(
+        { _id: id }, // Buscar por ID
+        { $set: { titulo, autor, fechapublicación, encabezado, Contenido, revisada, revisó, imagen, categoria } } 
+    )
+        .then((data) => res.json(data)) 
+        .catch((error) => res.status(500).json({ message: error.message })); 
+});
