9814dae7b35be40dfc56494eb2ca02a96a3729b7
Adrian <adriandavidcc@gmail.com>
2025-05-11 21:12:01 -0500

    Se soluciona un error, ya que no se estaba llamando al modulo de categoria en index. js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,20 @@ const parser = require("body-parser");
 const express = require('express');
 const app = express();
 const port = 3000;
+const categoriaRoutes = require("./routes/categoria"); 
 const mongoose = require("mongoose");
 require('dotenv').config();
 
 app.use(parser.urlencoded({ extended: false })); 
 app.use(parser.json()); 
+
+
+
+app.use("/api", categoriaRoutes); 
+
+
 app.use(express.json());
+
 mongoose
     .connect(process.env.MONGODB_URI)
     .then(() => console.log("Conexión exitosa"))
